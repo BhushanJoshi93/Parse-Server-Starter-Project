cbc462270f8ee7f544687c3ca56d627897da718a
Bhushan Joshi <joshi11p@uwindsor.ca>
2018-09-22 16:24:46 -0400

    Intialization of starter project done.

---
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Switch;
+
+import com.parse.GetCallback;
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+
+    //HOW TO CALL PARSE
+//    ParseObject score = new ParseObject("Score");
+//    score.put("Username", "Nick");
+//    score.put("score", "45");
+
+//    score.saveInBackground(new SaveCallback() {
+//      @Override
+//      public void done(ParseException e) {
+//        if (e == null){
+//          Log.i("Success","We saved the score Obecjt");
+//        }
+//        else {
+//          e.printStackTrace();
+//        }
+//      }
+//    });
+
+
+    //HOW TO GET SOMETHING FROM PARSE
+
+    ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
+    query.getInBackground("80DdVZZHgd", new GetCallback<ParseObject>() {
+      @Override
+      public void done(ParseObject object, ParseException e) {
+        if (e == null && object != null){
+
+          object.put("score",85);
+          object.saveInBackground();
+          Log.i("Username", object.getString("Username"));
+          Log.i("Score", String.valueOf(object.getInt("score")));
+
+        }
+        else {
+          e.printStackTrace();
+        }
+      }
+    });
+
+
+    ParseAnalytics.trackAppOpenedInBackground(getIntent());
+  }
+
+
+}
---
+++ ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    //got this from the terminal
+    Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+            .applicationId("9477273caae35d865ad79ddd2f53b9de47354972")
+            .clientKey("7dda1e871367623a8ccc33dabf6f0fbc1178c976")
+            .server("http://18.222.210.21:80/parse/")
+            .build()
+    );
+
+//    ParseObject object = new ParseObject("ExampleObject");
+//    object.put("myNumber", "123");
+//    object.put("myString", "rob");
+//
+//    object.saveInBackground(new SaveCallback () {
+//      @Override
+//      public void done(ParseException ex) {
+//        if (ex == null) {
+//          Log.i("Parse Result", "Successful!");
+//        } else {
+//          Log.i("Parse Result", "Failed" + ex.toString());
+//        }
+//      }
+//    });
+
+
+    ParseUser.enableAutomaticUser();
+
+    ParseACL defaultACL = new ParseACL();
+    defaultACL.setPublicReadAccess(true);
+    defaultACL.setPublicWriteAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+
+  }
+}
